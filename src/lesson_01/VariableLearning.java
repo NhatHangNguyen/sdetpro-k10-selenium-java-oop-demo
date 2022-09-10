b340ac4903c580467a18b4f3d92a2fdcf58f0423
Hang Chau Nhat Nguyen <hangcnnguyen@kms.com.vn>
2022-09-10 23:24:19 +0700

    Commit code day 1

---
+++ src/lesson_01/VariableLearning.java
@@ -0,0 +1,15 @@
+package lesson_01;
+
+public class VariableLearning {
+
+    public static void main(String[] args) {
+        int myIntNum = 18;
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+
+        // byte myByteNum = 128;
+
+        short myByteNum = 128;
+        System.out.println(Short.MAX_VALUE);
+    }
+}
