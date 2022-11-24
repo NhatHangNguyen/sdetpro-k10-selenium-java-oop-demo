34c6c02f749bb4e814857c0bf4f40da27a3b3073
Hang Chau Nhat Nguyen <hangcnnguyen@kms.com.vn>
2022-11-24 20:34:31 +0700

    Update

--- src/lesson_01/Hello.java
+++ src/lesson_01/Hello.java
@@ -1,6 +1,8 @@
 package lesson_01;
 
 public class Hello {
+
+    // Method signature | varargs | reset parameters
     public static void main(String[] args) {
         System.out.println("Hello!");
     }
