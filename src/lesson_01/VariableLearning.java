14e06784b3959e901f9f98c25358db7e540898e3
Hang Chau Nhat Nguyen <hangcnnguyen@kms.com.vn>
2023-01-30 21:00:56 +0700

    Update lession 01

--- src/lesson_01/VariableLearning.java
+++ src/lesson_01/VariableLearning.java
@@ -2,6 +2,7 @@ package lesson_01;
 
 // Class name => follow the rule: PascalCase (VariableLearning)
 // Variable name => snake_case (int my_num = 18; | in python) and camelCase (int myNum = 18; | in Java)
+
 public class VariableLearning {
 
     public static void main(String[] args) {
@@ -14,7 +15,7 @@ public class VariableLearning {
         System.out.println(Byte.MAX_VALUE);
         System.out.println(Byte.MIN_VALUE);
 
-        short myByteNum_1 = 128;
+        short myShortNum = 128;
         System.out.println(Short.MAX_VALUE);
         System.out.println(Short.MIN_VALUE);
 
@@ -26,13 +27,12 @@ public class VariableLearning {
         System.out.println(Float.MAX_VALUE);
         System.out.println(Float.MIN_VALUE);
 
-        double myDoubleNum = 3.14;
+        double myDoubleNum = 3.14D;
         System.out.println(Double.MAX_VALUE);
         System.out.println(Double.MIN_VALUE);
 
         final int MY_CONSTANT = 123;
         // MY_CONSTANT = 234; => cannot assign a value to final variable MY_CONSTANT, cannot re-assign
-
     }
 }
 
